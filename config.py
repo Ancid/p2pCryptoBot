3d0114957769dd76d91792159b38e9fd0669ba9f
sidorovanton <anton.sidorov@paxful.com>
2020-05-18 21:49:17 +0300

    Refactored to mongoDB

--- config.py
+++ config.py
@@ -1,7 +1,9 @@
+import os
+
+# os.environ['MONGODB_URI'] = 'mongodb://localhost:27017/offersBot'
 TOKEN = '959670109:AAHTx35TqKD5U2FX6lnX5frQ3-1s27YbMcA'
 APP_NAME = 'offers-bot'
 DB_NAME = 'db/offersStorage'
-DB_MONGO_DSN = 'mongodb://bot_db:heroku_tz78nkrk@ds115219.mlab.com:15219/heroku_tz78nkrk'
 API_KEY = "Z4TPCz2VKHTlRAMAXxLqgHS18MT7eLbd"
 API_SECRET = b"tdlPtrDbSM8jacK1I3VPNwNx7svXkaL0"
 OFFERS_LIMIT = 5
