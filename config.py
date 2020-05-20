a49062a62bb7e4f37b811d51c738b3e47319809e
sidorovanton <anton.sidorov@paxful.com>
2020-05-20 23:45:48 +0300

    CHANGE: fix app name

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
-# import os
+import os
 
-# os.environ['MONGODB_URI'] = 'mongodb://localhost:27017/offersBot'
+os.environ['MONGODB_URI'] = 'mongodb://localhost:27017/offersBot'
 TOKEN = '1275378588:AAF3KRJxWTCsR5RXtG4H6WRkybr4Gkk-g3Y'
-APP_NAME = 'offers-bot'
+APP_NAME = 'paxful-offers-bot'
 DB_NAME = 'db/offersStorage'
 API_KEY = "Z4TPCz2VKHTlRAMAXxLqgHS18MT7eLbd"
 API_SECRET = b"tdlPtrDbSM8jacK1I3VPNwNx7svXkaL0"
