fd34cf57a8321cd6af739a525978836cc95a6768
sidorovanton <anton.sidorov@paxful.com>
2020-05-20 23:16:42 +0300

    CHANGE: server conf

--- run_server.py
+++ run_server.py
@@ -35,5 +35,4 @@ def crone():
 
 
 if __name__ == "__main__":
-    server.run(debug=True)
-    # server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
+    server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
