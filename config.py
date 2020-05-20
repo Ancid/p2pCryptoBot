a099f32265d3cd51632ca132ec94e516db26295c
sidorovanton <anton.sidorov@paxful.com>
2020-05-20 23:25:08 +0300

    CHANGE: new bot

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 # import os
 
 # os.environ['MONGODB_URI'] = 'mongodb://localhost:27017/offersBot'
-TOKEN = '959670109:AAHTx35TqKD5U2FX6lnX5frQ3-1s27YbMcA'
+TOKEN = '1275378588:AAF2ue8ewLQtgWJelZyD6rgdB_upq1T6zv4'
 APP_NAME = 'offers-bot'
 DB_NAME = 'db/offersStorage'
 API_KEY = "Z4TPCz2VKHTlRAMAXxLqgHS18MT7eLbd"
