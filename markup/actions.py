5f266680fe0167c57c13d1fec9c0301d668e52f2
sidorovanton <anton.sidorov@paxful.com>
2020-05-22 17:44:04 +0300

    Improve bot

--- markup/actions.py
+++ markup/actions.py
@@ -5,8 +5,8 @@ def markup_actions(active=None):
     mp_actions = types.InlineKeyboardMarkup(row_width=3)
     if active == True:
         mp_actions.add(
-            types.InlineKeyboardButton(text="Offer search", callback_data="action:search"),
             types.InlineKeyboardButton(text="Change subscription", callback_data="action:subscribe"),
+            types.InlineKeyboardButton(text="Unsubscribe", callback_data="action:unsubscribe")
         )
     else:
         mp_actions.add(types.InlineKeyboardButton(text="Subscribe", callback_data="action:subscribe"))
