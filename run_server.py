d90a07a2ea8c0813962984e61579657f9392c6b3
sidorovanton <anton.sidorov@paxful.com>
2020-06-04 18:04:47 +0300

    Change server to aiohttp

--- run_server.py
+++ run_server.py
@@ -1,47 +1,82 @@
-import hashlib
-
-import flask
+# import flask
+# import os
 from telebot import types
 from config import *
 from bot import bot
-import os
+from aiohttp import web
 
-from mongo_db.db import db_users
 from subscriptions import walk_through_subsciptions
 
-server = flask.Flask(__name__)
+app = web.Application()
 
 
-@server.route('/' + TOKEN, methods=['POST'])
-def get_message():
-    bot.process_new_updates([types.Update.de_json(
-        flask.request.stream.read().decode("utf-8"))])
-    return "!", 200
+async def webhook(request):
+    request_body_dict = await request.json()
+    bot.process_new_updates([types.Update.de_json(request_body_dict)])
+    return web.Response()
 
 
-@server.route('/', methods=["GET"])
-def index():
-    return "Hello from OffersBot!", 200
+async def index():
+    return web.Response(text="Hello from OffersBot!")
 
 
-@server.route('/webhook', methods=["GET"])
-def webhook():
+async def reset_webhook():
     bot.remove_webhook()
     bot.set_webhook(url="https://{}.herokuapp.com/{}".format(APP_NAME, TOKEN))
-    return "Hello from OffersBot!", 200
+    return web.Response(text="Hello from OffersBot!")
 
 
-@server.route('/check/subscriptions', methods=["GET"])
-def crone():
+async def check_subscriptions():
     walk_through_subsciptions()
-    return "Done!", 200
+    return web.Response(text="Done")
+
+app.router.add_post("/{token}/", webhook)
+app.router.add_get("/", index)
+app.router.add_get("/reset_webhook", reset_webhook)
+app.router.add_get("/check/subscriptions", check_subscriptions)
+app.router.add_get("/alias", check_subscriptions)
 
 
-@server.route('/alias', methods=["GET"])
-def crone_alias():
-    walk_through_subsciptions()
-    return "Done!", 200
+web.run_app(
+    app,
+    host="0.0.0.0",
+    port=int(os.environ.get('PORT', 5000))
+)
+
+# --------------------------
+# server = flask.Flask(__name__)
 
 
-if __name__ == "__main__":
-    server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
+# @server.route('/' + TOKEN, methods=['POST'])
+# def get_message():
+#     bot.process_new_updates([types.Update.de_json(
+#         flask.request.stream.read().decode("utf-8"))])
+#     return "!", 200
+
+
+# @server.route('/', methods=["GET"])
+# def index():
+#     return "Hello from OffersBot!", 200
+#
+#
+# @server.route('/webhook', methods=["GET"])
+# def webhook():
+#     bot.remove_webhook()
+#     bot.set_webhook(url="https://{}.herokuapp.com/{}".format(APP_NAME, TOKEN))
+#     return "Hello from OffersBot!", 200
+#
+#
+# @server.route('/check/subscriptions', methods=["GET"])
+# def crone():
+#     walk_through_subsciptions()
+#     return "Done!", 200
+#
+#
+# @server.route('/alias', methods=["GET"])
+# def crone_alias():
+#     walk_through_subsciptions()
+#     return "Done!", 200
+#
+#
+# if __name__ == "__main__":
+#     server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
