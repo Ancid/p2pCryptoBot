5b35138038913a0e99327509064d7a4aad38c88c
sidorovanton <anton.sidorov@paxful.com>
2020-08-23 17:14:16 +0300

    Update db conn

--- config.py
+++ config.py
@@ -1,7 +1,9 @@
-# import os
-#
+import os
+
 # os.environ['MONGODB_URI'] = 'mongodb://localhost:27017/offersBot'
-# os.environ['DEBUG'] = 'True'
+# os.environ['MONGODB_ATLAS'] = 'mongodb+srv://paxbot-user:56ogVp8kncjxwtpK@cluster0.afujh.mongodb.net/paxbot'
+# os.environ['MONGODB_URI_NEW'] = 'mongodb://paxbot-user:56ogVp8kncjxwtpK@cluster0-shard-00-02.afujh.mongodb.net/paxbot'
+os.environ['DEBUG'] = 'True'
 # TOKEN = '1275378588:AAF3KRJxWTCsR5RXtG4H6WRkybr4Gkk-g3Y'
 TOKEN = '1206885593:AAH4kKVpyM-BdznBGmaHmriMfoF2PR49sBc'
 APP_NAME = 'paxful-offers-bot'
